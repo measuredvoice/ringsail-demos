508c0f7ab756ff896140530fe2c7d4252b1a1d17
reshun <reshun@gmail.com>
2013-02-15 16:30:31 -0800

    Added infinite scroll
    Google and Twitter widgets have been disabled by default as the number of
    that are required to load are bringing the browser to its knees especially
    when scrolling. To enable them uncomment the the '@show_shares' line in
    app/controllers/flickr_controller.rb
    
    Also, this required some changes to the way the twitter and google JS files were
    loaded. They have been moved to the end the page and are loaded synchronously as
    they are needed in order to load their respective widgets from within the page's
    JS.

--- app/assets/javascripts/flickr.js
+++ app/assets/javascripts/flickr.js
@@ -1,11 +1,68 @@
 $(function(){
   var $container = $('#photo-container');
-  
+
+  var showShareres = function(event, visible){
+    $target = $(event.target);
+    if (visible == true) {
+      twttr.widgets.load();
+      gapi.plusone.render($target.find('.g-plusone').get(0), {"size":"medium", "annotation": "none"});
+    }
+  };
+
+  // hide photo items
+  $container.find(".photo-item").css({ opacity: 0 });
+
+  // hide page navigation
+  $('.pagination').hide();
+
+  // load images
   $container.imagesLoaded( function(){
+    var $items = this.find('.photo-item');
+
+    // apply masonry
     $container.masonry({
       itemSelector : '.photo-item',
-      isAnimated: true,
       columnWidth: 320
     });
+
+    // display the items
+    $items.animate({opacity: 1});
+
+    // add sharer show event listener
+    $items.find('.share-buttons.with-google-twitter').one('inview', showShareres);
+    $(window).scroll();
   });
+
+  // setup infinite scroll
+  $container.infinitescroll({
+    // debug                 : true,
+    navSelector           : 'div.pagination',
+    nextSelector          : '.pagination a.next-page',
+    itemSelector          : 'div.photo-item',
+    pixelsFromNavToBottom : 50,
+    bufferPx: 800,
+    loading: {
+        finishedMsg: 'No more pages to load.',
+        msgText: "<em>Loading...</em>"
+      }
+    },
+    // trigger Masonry as a callback
+    function( newElements ) {
+      // hide new items while they are loading
+      var $newElems = $( newElements ).css({ opacity: 0 });
+
+      // ensure that images load before adding to masonry layout
+      $newElems.imagesLoaded(function(){
+        // show new elems now that they're ready
+        $newElems.animate({ opacity: 1 });
+
+        // apply masonry
+        $container.masonry( 'appended', $newElems, true );
+
+        // add sharer show event listener
+        this.find('.share-buttons.share-buttons.with-google-twitter').one('inview', showShareres);
+        $(window).scroll();
+      });
+    }
+  );
 });
--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,30 +1,31 @@
 class FlickrController < ApplicationController
   layout 'flickr'
-  
+
   def show
     @photos = []
-    page_size = params[:page_size] || 30
-    
+    page_size = params[:page_size].to_i > 0 ? params[:page_size].to_i : 15
     account_options = {:service_id => 'flickr'}
     registry = Registry::Client.new
     @registry_accounts = registry.accounts(account_options).map do |account|
       # Turn the Flickr URL into a standard ID
       FlickrPhoto.find_account_id(account['service_url'], account['account'])
     end.find_all {|x| x.present?}
-    
+
     # registry_accounts = ["51986662@N05", "51009184@N06", "46577594@N04"]
-    
+
     all_photos = FlickrPhoto.list_for_accounts(@registry_accounts)
-    
+
     @photo_count = all_photos.count
-    
+
     # Cheap paging through an assumed-large dataset
     @this_page = params[:page].present? ? params[:page].to_i : 1
     @next_page = @this_page + 1
     @prev_page = @this_page - 1
     @total_pages = @photo_count / page_size + 1
-    offset = (@this_page - 1) * page_size 
+    offset = (@this_page - 1) * page_size
     @photos = all_photos[offset, page_size]
+
+    # uncomment the next line to show g+ and tweet sharers
+    # @show_sharers = 1
   end
-  
 end
